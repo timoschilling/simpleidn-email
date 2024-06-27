4e5eece60a8b17e939183ebe3e5decaedcd847b0
Timo Schilling <timo@schilling.io>
2024-06-27 20:33:47 +0200

    init

---
+++ lib/simpleidn/email.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "email/version"
+
+module Simpleidn
+  module Email
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/simpleidn/email/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Simpleidn
+  module Email
+    VERSION = "0.1.0"
+  end
+end
